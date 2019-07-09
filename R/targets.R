a42551ac69d5c983e7d30a94b47cb8c67081ae10
Jesse Piburn <jesse.piburn@gmail.com>
2019-07-09 16:48:04 -0400

    add fp_targets()

--- R/targets.R
+++ R/targets.R
@@ -1,48 +1,37 @@
-#' NFL Snap Counts by Week
+#' Target Counts by Week
 #'
 #' @param pos \code{Charcater}. Specific position you want to return. Default will return all
 #'            all offensive postions. Available options include
 #'            \itemize{
 #'             \item \code{"offense"}
-#'             \item \code{"QB"}
 #'             \item \code{"RB"}
 #'             \item \code{"WR"}
 #'             \item \code{"TE"}
-#'             \item \code{"Defense"}
-#'             \item \code{"DL"}
-#'             \item \code{"LB"}
-#'             \item \code{"DB"}
 #'            }
 #' @param season \code{Numeric}. The NFL season. If missing it will return the current year's season.
-#'               Supported season only go back to \code{2016}
-#' @param percentage \code{Logical}. Return counts as percentages? Default is \code{FALSE}
+#'               Supported season only go back to \code{2013}
 #'
 #' @return a tibble
 #' @export
 #'
 #' @note \itemize{
-#'        \item Due to how fantasypros returns the data, all player's teams are listed under their
-#'       current team regardless of what season you query.
-#'       \item Bye weeks are represented as \code{NA}
+#'       \item Bye weeks and games players did not play in are represented as \code{NA}
 #'        }
 #'
 #' @examples
 #'
 #' # all offensive positions for the 2018 season
-#' fp_snaps(season = 2018)
+#' fp_targets(season = 2018)
 #'
-#' # Percentage of total game snaps for TE's in the 2018 season
-#' fp_snaps(pos = "TE", season = 2018, percentage = TRUE)
+#' # total targets for TE's in the 2014 season
+#' fp_targets(pos = "TE", season = 2014)
 #'
-#' # all defensive positions for the current NFL season.
-#' fp_snaps(pos = "defense")
-fp_snaps <- function(pos = "offense", season, percentage = FALSE) {
+#' # all WR targets for the current NFL season.
+#' fp_targets(pos = "WR")
+fp_targets <- function(pos = "offense", season) {
 
   if (missing(pos)) pos <- "offense"
   else pos <- tolower(pos)
-
-  if (percentage) perc_url <- "show=perc"
-  else perc_url <- "show=count"
 
   if (missing(season)) season <- as.numeric(format(Sys.Date(), "%Y"))
 
@@ -51,26 +40,21 @@ fp_snaps <- function(pos = "offense", season, percentage = FALSE) {
   pos_url <- switch(pos,
                     "offense" = "",
                     "off"     = "",
-                    "qb"      = "qb.php",
                     "rb"      = "rb.php",
                     "wr"      = "wr.php",
-                    "te"      = "te.php",
-                    "def"     = "defense.php",
-                    "defense" = "defense.php",
-                    "dl"      = "dl.php",
-                    "lb"      = "lb.php",
-                    "db"      = "db.php")
+                    "te"      = "te.php"
+                    )
 
   if (is.null(pos_url)) {
     warning(
-      paste("position", pos, "not recognized. Defaulting to offense only."),
+      paste("position", pos, "not recognized. Defaulting to all offensive positions"),
       call. = FALSE
     )
     pos_url <- ""
   }
 
   fp_url <- paste0(
-    "https://www.fantasypros.com/nfl/reports/snap-counts/",
+    "https://www.fantasypros.com/nfl/reports/targets/",
     pos_url, "?", season_url, "&", perc_url
   )
 
